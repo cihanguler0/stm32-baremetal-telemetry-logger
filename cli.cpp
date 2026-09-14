d245713249f56660ab573905c4b5e07e506324a9
b2230356069 <b2230356069@cs.hacettepe.edu.tr>
2026-09-14 09:51:34 +0300

    Update CLI state handling

--- cli.cpp
+++ cli.cpp
@@ -42,20 +42,26 @@ void CLI_Process(void) {
                 int matched = sscanf(cmd_line, "%15s %d", cmd, &value);
 
                 if (matched > 0) {
+
                     if (strcmp(cmd, "SET_PWM") == 0) {
+
                         if (s_ctx->current_state == STATE_CRITICAL_TEMP) {
                             printf("ERROR: CANNOT CHANGE PWM IN CRITICAL TEMP\n");
+
                         } else if (matched == 2 && value >= 0 && value <= 255) {
                             s_ctx->pwm_value = (uint8_t)value;
                             printf("PWM HAS SET: %d\n", s_ctx->pwm_value);
+
                         } else {
                             printf("UNDEFINED PWM VALUE (0-255)\n");
                         }
                     }
                     else if (strcmp(cmd, "LOG_INTERVAL") == 0) {
+
                         if (matched == 2 && value > 0) {
                             s_ctx->log_interval_ms = (uint32_t)value;
                             printf("LOG INTERVAL HAS UPDATED TO %u ms\n", s_ctx->log_interval_ms);
+
                         } else {
                             printf("TIME HAS TO BE GREATER THAN 0\n");
                         }
@@ -88,8 +94,10 @@ void CLI_Process(void) {
             cmd_index = 0;
         }
         else if (rx_byte != '\r') {
+
             if (cmd_index < (sizeof(cmd_line) - 1)) {
                 cmd_line[cmd_index++] = rx_byte;
+                
             } else {
                 printf("OVERFLOW!\n");
                 overflow_flag = true;
